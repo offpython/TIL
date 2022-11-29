7b97393673bc5447ee458664228a5f5561a618b5
offpython <offpython@gmail.com>
2022-11-29 15:44:25 +0900

    Update

--- SQL/03_Date, Timestamp, Interval 다루기/날짜와 시간 연산. interval의 활용_실습.sql
+++ SQL/03_Date, Timestamp, Interval 다루기/날짜와 시간 연산. interval의 활용_실습.sql
@@ -22,15 +22,17 @@ select to_date('2022-01-01', 'yyyy-mm-dd') + interval '2 days' as date_01;
 select to_date('2022-01-01', 'yyyy-mm-dd') + interval '2' day as date_01;
 
 -- 날짜 간의 차이 구하기. 차이값은 정수형.  
+select to_date('2022-01-01', 'yyyy-mm-dd') - to_date('2022-01-03', 'yyyy-mm-dd') as interval_01
+	, pg_typeof(to_date('2022-01-03', 'yyyy-mm-dd') - to_date('2022-01-01', 'yyyy-mm-dd')) as type ;
+
 select to_date('2022-01-03', 'yyyy-mm-dd') - to_date('2022-01-01', 'yyyy-mm-dd') as interval_01
 	, pg_typeof(to_date('2022-01-03', 'yyyy-mm-dd') - to_date('2022-01-01', 'yyyy-mm-dd')) as type ;
 
 -- Timestamp간의 차이 구하기. 차이값은 interval 
 select to_timestamp('2022-01-01 14:36:52', 'yyyy-mm-dd hh24:mi:ss') 
-     - to_timestamp('2022-01-01 12:36:52', 'yyyy-mm-dd hh24:mi:ss') as time_01
+     - to_timestamp('2022-01-03 12:36:52', 'yyyy-mm-dd hh24:mi:ss') as time_01
      , pg_typeof(to_timestamp('2022-01-01 08:36:52', 'yyyy-mm-dd hh24:mi:ss') 
-     - to_timestamp('2022-01-01 12:36:52', 'yyyy-mm-dd hh24:mi:ss')) as type
-;
+     - to_timestamp('2022-01-01 12:36:52', 'yyyy-mm-dd hh24:mi:ss')) as type ;
 
 -- date + date는 허용하지 않음. 
 select to_date('2022-01-03', 'yyyy-mm-dd') +  to_date('2022-01-01', 'yyyy-mm-dd')
@@ -44,69 +46,10 @@ select empno, ename, hiredate, now(), current_timestamp, current_date, current_t
 	, now() - hiredate as 근속기간
 from hr.emp
 )
-select *
+select empno, ename, hiredate, now() - hiredate as 근속기간
 	, date_part('year', 근속기간)
-	, justify_interval(근속기간)
-	, age(hiredate)
+	, justify_interval(근속기간)  -- 한달 30일 기준으로 함 
+	, age(hiredate)  -- 날짜 정확히 계산 
 	, date_part('year', justify_interval(근속기간))||'년 '||date_part('month', justify_interval(근속기간))||'월' as 근속년월
 	, date_part('year', age(hiredate))||'년 '||date_part('month', age(hiredate))||'월' as 근속년월_01
 from temp_01;
-
-
-
-
-/******************************************************
-date_trunc 함수를 이용하여 년/월/일/시간/분/초 단위 절삭 
-*******************************************************/
-
-select trunc(99.9999, 2);
-
---date_trunc는 인자로 들어온 기준으로 주어진 날짜를 절삭(?),
-select date_trunc('day', '2022-03-03 14:05:32'::timestamp)
-
--- date타입을 date_trunc해도 반환값은 timestamp타입임. 
-select date_trunc('day', to_date('2022-03-03', 'yyyy-mm-dd')) as date_01;
-
--- 만약 date 타입을 그대로 유지하려면 ::date로 명시적 형변환 
-select date_trunc('day', '2022-03-03'::date)::date as date_01
-
--- 월, 년으로 절단. 
-select date_trunc('month', '2022-03-03'::date)::date as date_01;
-
--- week의 시작 날짜 구하기. 월요일 기준.
-select date_trunc('week', '2022-03-03'::date)::date as date_01;
-
--- week의 마지막 날짜 구하기. 월요일 기준(일요일이 마지막 날짜)
-select (date_trunc('week', '2022-03-03'::date) + interval '6 days')::date as date_01;
-
--- week의 시작 날짜 구하기. 일요일 기준.
-select date_trunc('week', '2022-03-03'::date)::date -1 as date_01;
-
--- week의 마지막 날짜 구하기. 일요일 기준(토요일이 마지막 날짜)
-select (date_trunc('week', '2022-03-03'::date)::date - 1 + interval '6 days')::date as date_01;
-
--- month의 마지막 날짜 
-select (date_trunc('month', '2022-03-03'::date) + interval '1 month' - interval '1 day')::date;
-
--- 시분초도 절삭 가능. 
-select date_trunc('hour', now());
-
---date_trunc는 년, 월, 일 단위로 Group by 적용 시 잘 사용됨.
-drop table if exists hr.emp_test;
-
-create table hr.emp_test
-as
-select a.*, hiredate + current_time
-from hr.emp a;
-
-select * from hr.emp_test;
-
--- 입사월로 group by
-select date_trunc('month', hiredate) as hire_month, count(*)
-from hr.emp_test
-group by date_trunc('month', hiredate);
-
--- 시분초가 포함된 입사일일 경우 시분초를 절삭한 값으로 group by 
-select date_trunc('day', hiredate) as hire_day, count(*)
-from hr.emp_test
-group by date_trunc('day', hiredate);
