4b6904cf60314f379d0ab7b14d8d7fddbbdf198d
offpython <offpython@gmail.com>
2022-12-05 14:47:53 +0900

    revised

--- SQL/05_Analytics SQL -개요와 순위 Analytics/순위함수(null처리)_실습2.sql
+++ SQL/05_Analytics SQL -개요와 순위 Analytics/순위함수(null처리)_실습2.sql
@@ -1,3 +1,4 @@
+-- -- 회사내 커미션 높은 순위. rank와 row_number 모두 추출. 
 -- null을 가장 선두 순위로 처리
 select a.*
 	, rank() over (order by comm desc nulls first ) as comm_rank
@@ -10,14 +11,8 @@ select a.*
 	, row_number() over (order by comm desc nulls last) as comm_rnum
 from hr.emp a;
 
--- null을 전처리하여 순위 정함. 
+-- null을 전처리하여 순위 정함. (null값일때 0으로 지)
 select a.*
 	, rank() over (order by COALESCE(comm, 0) desc ) as comm_rank
 	, row_number() over (order by COALESCE(comm, 0) desc) as comm_rnum
 from hr.emp a;
-
-select a.*
-	, rank() over (order by comm desc) as comm_rank
-	, row_number() over (order by comm desc) as comm_rnum
-from hr.emp a;
-
